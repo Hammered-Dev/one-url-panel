be350e61c8e6191cae8a0df7555f16ee06dca1b8
Timothy0508 <timothytseng508@gmail.com>
2025-08-11 18:04:37 +0800

    feat: update app title and improve loading/error display in UrlList component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,11 +14,13 @@ function UrlList() {
   useEffect(
     () => {
       const fetch = async () => {
+        const url = `${process.env.API_HOST}:${process.env.API_PORT}/manage/urls`
         try {
-          const response = await axios.get(`${process.env.API_HOST}:${process.env.API_PORT}/manage/urls`);
+          const response = await axios.get(url);
           setData(response.data)
         } catch (err) {
           setError(err)
+          console.log(url)
         } finally {
           setloading(false)
         }
@@ -30,11 +32,13 @@ function UrlList() {
 
   if (loading) {
     return (
-      <div>Loading...</div>
+      <div className="items-center max-w-sm">
+        Loading...
+      </div>
     )
   } else if (error) {
     return (
-      <div>
+      <div className="items-center max-w-sm">
         Error(s) has occurs: {`${error}`}
       </div>
     )
@@ -51,7 +55,7 @@ export default function Home() {
   return (
     <>
       <header className="flex items-center gap-4 rounded-2xl p-2 m-1 shadow-lg outline outline-black/5 dark:outline-white/5 dark:bg-slate-800 dark:outline-offset-1 dark:shadow-none">
-        <Image src={"/ou.svg"} alt="The app icon" width={40} height={40}></Image>
+        <Image src={"/ou.svg"} alt="The app icon" width={32} height={32}></Image>
         <h1 className="text-2xl">
           OneUrl
         </h1>
