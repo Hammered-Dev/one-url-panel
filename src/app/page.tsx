047dc6095c15c535c3c4106d42f523741a0515b8
Timothy0508 <timothytseng508@gmail.com>
2025-08-11 15:11:43 +0800

    feat: implement URL fetching and display with loading and error handling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,62 @@
-import axios from "axios";
-import process from "process";
+"use client"
 
-// function to fetch data from an API
-export async function fetchData(url: string) {
-  try {
-    const response = await axios.get(url);
-    return response.data;
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    throw error;
+import axios, { AxiosResponse } from "axios";
+import process from "process";
+import Image from 'next/image';
+import React, { useEffect, useState } from "react";
+import "./globals.css"
+
+function UrlList() {
+  const [data, setData] = useState<AxiosResponse | null>(null);
+  const [error, setError] = useState<unknown | null>(null);
+  const [loading, setloading] = useState(true);
+
+  useEffect(
+    () => {
+      const fetch = async () => {
+        try {
+          const response = await axios.get(`${process.env.API_HOST}:${process.env.API_PORT}/manage/urls`);
+          setData(response.data)
+        } catch (err) {
+          setError(err)
+        } finally {
+          setloading(false)
+        }
+      }
+
+      fetch()
+    }, []
+  )
+
+  if (loading) {
+    return (
+      <div>Loading...</div>
+    )
+  } else if (error) {
+    return (
+      <div>
+        Error(s) has occurs: {`${error}`}
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        {`${data}`}
+      </div>
+    )
   }
 }
 
-export default async function Home() {
-  const short_urls = fetchData(`${process.env.API_HOST}:${process.env.API_PORT}/manage/urls`);
-
+export default function Home() {
   return (
     <>
-      <h1>Short URLs</h1>
-      <ul>
-        {short_urls.then((res) => {
-          for (let index = 0; index < res.length; index++) {
-            const element = res.data[index];
-            console.log(res)
-            console.table(res.data)
-            return (
-              <li>
-                {element["target"]}
-              </li>
-            )
-          }
-        })
-        }
-      </ul>
+      <header className="flex items-center gap-4 rounded-2xl p-2 m-1 shadow-lg outline outline-black/5 dark:outline-white/5 dark:bg-slate-800 dark:outline-offset-1 dark:shadow-none">
+        <Image src={"/ou.svg"} alt="The app icon" width={40} height={40}></Image>
+        <h1 className="text-2xl">
+          OneUrl
+        </h1>
+      </header>
+      <UrlList></UrlList>
     </>
   );
 }
