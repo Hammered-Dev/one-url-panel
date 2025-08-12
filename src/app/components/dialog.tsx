833e0c4a8e284f53d10206a8a4a4016f988c87eb
Timothy0508 <timothytseng508@gmail.com>
2025-08-12 10:06:23 +0800

    feat: add AppBar and Dialog components to enhance UI and user interaction in Home

---
+++ src/app/components/dialog.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import { useSearchParams } from "next/navigation"
+import { JSX, useEffect, useRef } from "react"
+
+type Props = {
+    title: string,
+    onClose: () => void,
+    onComfirm: () => void,
+    children: React.ReactNode
+}
+
+export default function Dialog({ title, onClose, onComfirm, children }: Props) {
+    const params = useSearchParams();
+    const ref = useRef<HTMLDialogElement | null>(null);
+
+    const showDialog = params.get("dialog");
+
+    useEffect(
+        () => {
+            if (showDialog === 'true') {
+                ref.current?.showModal();
+            } else {
+                ref.current?.close();
+            }
+        }, [showDialog]
+    )
+
+    const close = () => {
+        ref.current?.close();
+        onClose();
+    }
+
+    const comfirm = () => {
+        ref.current?.close();
+        onComfirm();
+    }
+
+    const dialog: JSX.Element | null = showDialog === "true"
+        ? (
+            <dialog ref={ref} className="w-3xl rounded-2xl self-center place-self-center">
+                <div className="flex flex-col">
+                    <div className="p-2">
+                        <h1 className="text-xl">{title}</h1>
+                    </div>
+                    <div>{children}</div>
+                    <div className="flex flex-row justify-end mr-1">
+                        <button onClick={comfirm} className="m-1 rounded-xl hover:bg-black/10 p-2">
+                            Comfirm
+                        </button>
+                        <button onClick={close} className="m-1 rounded-xl hover:bg-black/10 p-2">
+                            Close
+                        </button>
+                    </div>
+                </div>
+            </dialog>
+        ) : null;
+
+    return dialog
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import "./globals.css"
 import { faGear, faPlus } from '@fortawesome/free-solid-svg-icons';
 import axios from 'axios';
+import Dialog from './components/dialog';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import AppBar from './components/appbar';
 
 export default function Home() {
   const [urlItems, setUrlItems] = useState<JSX.Element | null>(null)
+  const [targetUrl, setTargetUrl] = useState<string>("")
+  const route = useRouter()
+  const searchParams = useSearchParams()
+  const path = usePathname()
 
   useEffect(() => {
     const API_URL = process.env.NEXT_PUBLIC_API_URL
@@ -34,25 +41,57 @@ export default function Home() {
     ).catch((e) => setUrlItems(<div>{`${e}`}</div>))
   }, [])
 
+  function updateParams(key: string, value: string | null) {
+    const params = new URLSearchParams(searchParams);
+    if (!value) {
+      params.delete(key)
+    } else {
+      params.set(key, value)
+    }
+    route.replace(`${path}?${params.toString()}`)
+  }
+
+  function comfirmDialog() {
+    updateParams('dialog', null)
+    route.refresh()
+    setTargetUrl("")
+  }
+
+  function closeDialog() {
+    updateParams('dialog', null)
+    setTargetUrl("")
+  }
+
+  const handleChange = (event) => {
+    setTargetUrl(event.target.value);
+  };
+
   return (
     <>
-      <header className="flex items-center justify-between gap-4 rounded-2xl p-2 m-1 shadow-lg outline outline-black/5 dark:outline-white/5 dark:bg-slate-800 dark:outline-offset-1 dark:shadow-none">
-        <div className="flex items-center gap-4 ml-1">
-          <Image src={"/ou.svg"} alt="The app icon" width={32} height={32} />
-          <h1 className="text-2xl">
-            OneUrl
-          </h1>
+      <Dialog title='te' onClose={() => closeDialog()} onComfirm={() => comfirmDialog}>
+        <div className='m-3 gap-3 flex flex-col items-center'>
+          <div className='flex flex-row items-center w-full g-3'>
+            <div>Target:</div>
+            <div className='flex flex-row w-full items-center gap-2 ml-2 outline rounded-md pl-1 outline-black/15'>
+              <div>
+                {`${process.env.NEXT_PUBLIC_API_URL}`}/rd/
+              </div>
+              <input onChange={handleChange} value={targetUrl} className='w-full outline-transparent p-1 pl-2 rounded-md bg-black/10'></input>
+            </div>
+          </div>
         </div>
+      </Dialog>
+      <AppBar title='OneUrl' leading_icon='/ou.svg' actions={
         <div className='flex items-center gap-3'>
-          <button className='flex items-center gap-1.5 text-black/75 rounded-md h-8 bg-green-200 pr-2 pl-2 hover:bg-green-400 hover:cursor-pointer'>
+          <button onClick={() => updateParams('dialog', 'true')} className='flex items-center gap-1.5 text-black/75 rounded-md h-8 bg-green-200 pr-2 pl-2 hover:bg-green-400 hover:cursor-pointer'>
             <FontAwesomeIcon icon={faPlus} />
             Create
           </button>
-          <button className="rounded w-8 h-8 hover:bg-gray-300 hover:cursor-pointer">
+          <button onClick={() => console.log('setting')} className="rounded w-8 h-8 hover:bg-gray-300 hover:cursor-pointer">
             <FontAwesomeIcon icon={faGear} />
           </button>
         </div>
-      </header>
+      } />
       <div className='m-1'>
         {urlItems}
       </div>
