3e7b17f77d78029a5275b8328d45b8df8828a583
Timothy0508 <timothytseng508@gmail.com>
2025-08-16 14:30:15 +0800

    chore: install hero ui and make nessery changes to fit it's need

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { PublicEnvScript } from "next-runtime-env";
+import { Providers } from "./providers";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -31,7 +32,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Providers>
+          {children}
+        </Providers>
       </body>
     </html>
   );
